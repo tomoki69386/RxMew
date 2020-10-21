86e0546da5cfeaffe849d5c2bdca534bfddd497f
tomoki_sun <tomoki69386@gmail.com>
2020-10-21 13:25:28 +0900

    Update Mew version and target dependencies

--- Package.swift
+++ Package.swift
@@ -5,21 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "RxMew",
-    products: [
-        .library(
-            name: "RxMew",
-            targets: ["RxMew"]),
+    products: [.library(name: "RxMew", targets: ["RxMew"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/tomoki69386/Mew", from: "0.3.0"),
+        .package(url: "https://github.com/tomoki69386/Mew", from: "0.4.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.1.1")
     ],
     targets: [
-        .target(
-            name: "RxMew",
-            dependencies: []),
-        .testTarget(
-            name: "RxMewTests",
-            dependencies: ["RxMew"]),
+        .target(name: "RxMew", dependencies: ["Mew", "RxSwift"]),
+        .testTarget(name: "RxMewTests", dependencies: ["RxMew"]),
     ]
 )
